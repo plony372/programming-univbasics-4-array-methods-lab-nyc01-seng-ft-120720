a21fbc313dba51243c516a65376f360f71445e78
plony benav <menachemfuterfas@gmail.com>
2020-11-12 00:45:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,19 +3,19 @@ array.include?(element)
 end
 
 def using_sort(array)
-
+  array.sort(array)
 end
 
 def using_reverse(array)
-
+  array.reverse(array)
 end
 
 def using_first(array)
-
+  array.first(array)
 end
 
 def using_last(array)
-
+  array[-1]
 end
 
 def using_size(array)
